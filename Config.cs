191a63a059a9c7c3eae82a014b530bc970121f6f
OverQuartzer <421563926@qq.com>
2020-07-23 17:46:30 +0800

    添加项目文件。

---
+++ Config.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServerAspNetIdentity
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                   new IdentityResource[]
+                   {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                   };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("scope1"),
+                new ApiScope("scope2"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                //// m2m client credentials flow client
+                //new Client
+                //{
+                //    ClientId = "m2m.client",
+                //    ClientName = "Client Credentials Client",
+
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                //    AllowedScopes = { "scope1" }
+                //},
+
+                //// interactive client using code flow + pkce
+                //new Client
+                //{
+                //    ClientId = "interactive",
+                //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                //    AllowedGrantTypes = GrantTypes.Code,
+
+                //    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                //    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                //    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                //    AllowOfflineAccess = true,
+                //    AllowedScopes = { "openid", "profile", "scope2" }
+                //},
+
+                new Client()
+                {
+                   //客户端Id
+                    ClientId="apiClientImpl",
+                    ClientName="ApiClient for Implicit",
+                    //客户端授权类型，Implicit:隐藏模式
+                    AllowedGrantTypes=GrantTypes.Implicit,
+                    //允许登录后重定向的地址列表，可以有多个
+                   RedirectUris = {"http://localhost:30002/auth.html"},
+                    //允许访问的资源
+                    AllowedScopes={
+                       "secretapi",
+                       IdentityServerConstants.StandardScopes.OpenId,
+                       "PhoneModel"
+                   },
+                    //允许将token通过浏览器传递
+                    AllowAccessTokensViaBrowser=true,
+                    //允许ID_TOKEN附带Claims
+                    AlwaysIncludeUserClaimsInIdToken=true
+                },
+                new Client()
+               {
+                    AlwaysIncludeUserClaimsInIdToken=true,
+                   //客户端Id
+                    ClientId="apiClientHybrid",
+                    ClientName="ApiClient for HyBrid",
+                    //客户端密码
+                    ClientSecrets={new Secret("apiSecret".Sha256()) },
+                    //客户端授权类型，Hybrid:混合模式
+                    AllowedGrantTypes=GrantTypes.Hybrid,
+                    //允许登录后重定向的地址列表，可以有多个
+                   RedirectUris = {"https://localhost:30002/auth.html"},
+                    //允许访问的资源
+                    //允许访问的资源
+                    AllowedScopes={
+                       "secretapi",
+                       IdentityServerConstants.StandardScopes.OpenId,
+                       "PhoneModel"
+                   },
+                     AllowOfflineAccess = true,
+                    AllowAccessTokensViaBrowser=true
+               }
+
+
+            };
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,145 @@
+﻿using System.Reflection;
+using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServerAspNetIdentity.Data;
+using IdentityServerAspNetIdentity.Models;
+using IdentityServerHost.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
+namespace IdentityServerAspNetIdentity
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddMvc();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+
+
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            const string connectionString = @"Data Source=identity.db;";
+
+            var builder = services.AddIdentityServer()
+                .AddTestUsers(TestUsers.Users)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlite(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlite(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddAspNetIdentity<ApplicationUser>();
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                    // register your IdentityServer with Google at https://console.developers.google.com
+                    // enable the Google+ API
+                    // set the redirect URI to https://localhost:5001/signin-google
+                    options.ClientId = "copy client ID from Google here";
+                    options.ClientSecret = "copy client secret from Google here";
+                });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            InitializeDatabase(app);
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiScopes.Any())
+                {
+                    foreach (var apiScope in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(apiScope.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                //ApiScopes
+                //if (!context.ApiResources.Any())
+                //{
+                //    foreach (var resource in Config.Apis)
+                //    {
+                //        context.ApiResources.Add(resource.ToEntity());
+                //    }
+                //    context.SaveChanges();
+                //}
+            }
+        }
+    }
+}
